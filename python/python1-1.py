80d514b42cb0da8658f20e79b6677db34a2d781a
lostxcq <15868151289@163.com>
2017-06-12 01:11:16 +0800

    添加课后练习

---
+++ python/python1-1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Python基础-数据类型和变量
+练习:
+请打印出以下变量的值：
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+"""
+# 第一种
+print('n = 123')
+print('f = 456.789')
+print('s1 = \'Hello, world\'')
+print('s2 = \'Hello, \\\'Adam\\\'\'')
+print('s3 = r\'Hello, \"Bart\"\'')
+print('s4 = r\'\'\'Hello,\nLisa!\'\'\'')
+
+# 第二种
+print("""n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''""")
